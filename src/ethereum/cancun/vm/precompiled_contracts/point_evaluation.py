b1f04cea483beb73902b7f856013bb94415530b7
fselmo <fselmo2@gmail.com>
2024-08-06 11:27:34 -0600

    swap consensus-specs for ckzg

--- src/ethereum/cancun/vm/precompiled_contracts/point_evaluation.py
+++ src/ethereum/cancun/vm/precompiled_contracts/point_evaluation.py
@@ -11,21 +11,31 @@ Introduction
 
 Implementation of the POINT EVALUATION precompiled contract.
 """
-from eth2spec.deneb.mainnet import (
-    KZGCommitment,
-    kzg_commitment_to_versioned_hash,
+
+import hashlib
+import os
+
+from ckzg import (
+    load_trusted_setup,
     verify_kzg_proof,
 )
 
-from ethereum.base_types import U256, Bytes
-
+from ..gas import GAS_POINT_EVALUATION, charge_gas
 from ...vm import Evm
 from ...vm.exceptions import KZGProofError
-from ...vm.gas import GAS_POINT_EVALUATION, charge_gas
 
 FIELD_ELEMENTS_PER_BLOB = 4096
 BLS_MODULUS = 52435875175126190479447740508185965837690552500527637822603658699938581184513  # noqa: E501
 VERSIONED_HASH_VERSION_KZG = b"\x01"
+
+# load path from ../_utils/kzg_trusted_setup.txt
+TRUSTED_SETUP_PATH = os.path.join(
+    os.path.dirname(__file__), "..", "_utils", "kzg_trusted_setup.txt"
+)
+
+
+def kzg_to_versioned_hash(commitment: bytes) -> bytes:
+    return VERSIONED_HASH_VERSION_KZG + hashlib.sha256(commitment).digest()[1:]
 
 
 def point_evaluation(evm: Evm) -> None:
@@ -39,6 +49,8 @@ def point_evaluation(evm: Evm) -> None:
         The current EVM frame.
 
     """
+    charge_gas(evm, GAS_POINT_EVALUATION)
+
     data = evm.message.data
     if len(data) != 192:
         raise KZGProofError
@@ -46,26 +58,23 @@ def point_evaluation(evm: Evm) -> None:
     versioned_hash = data[:32]
     z = data[32:64]
     y = data[64:96]
-    commitment = KZGCommitment(data[96:144])
+    commitment = data[96:144]
     proof = data[144:192]
 
-    # GAS
-    charge_gas(evm, GAS_POINT_EVALUATION)
-    if kzg_commitment_to_versioned_hash(commitment) != versioned_hash:
+
+    # Verify commitment matches versioned_hash
+    if kzg_to_versioned_hash(commitment) != versioned_hash:
         raise KZGProofError
 
     # Verify KZG proof with z and y in big endian format
     try:
-        kzg_proof_verification = verify_kzg_proof(commitment, z, y, proof)
-    except Exception as e:
+        assert verify_kzg_proof(
+            commitment, z, y, proof, load_trusted_setup(TRUSTED_SETUP_PATH)
+        )
+    except (AssertionError, RuntimeError) as e:
         raise KZGProofError from e
 
-    if not kzg_proof_verification:
-        raise KZGProofError
-
-    # Return FIELD_ELEMENTS_PER_BLOB and BLS_MODULUS as padded
-    # 32 byte big endian values
-    evm.output = Bytes(
-        U256(FIELD_ELEMENTS_PER_BLOB).to_be_bytes32()
-        + U256(BLS_MODULUS).to_be_bytes32()
-    )
+    # Return FIELD_ELEMENTS_PER_BLOB and BLS_MODULUS as padded 32 byte big endian values
+    evm.output = FIELD_ELEMENTS_PER_BLOB.to_bytes(
+        32, "big"
+    ) + BLS_MODULUS.to_bytes(32, "big")
